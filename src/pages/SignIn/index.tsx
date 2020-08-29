5ff8d978769ec686589837a30f8290e816d26693
Mhayk Whandson <hi@mhayk.com>
2020-08-29 17:11:48 +0100

    The navigation with useNavigation hook has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -11,6 +12,7 @@ import { Container, Title, ForgotPassword, ForgotPasswordText,
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
   return (
     <>
       <KeyboardAvoidingView
@@ -40,7 +42,7 @@ const SignIn: React.FC = () => {
           </Container>
         </ScrollView>
 
-        <CreateAccountButton onPress={() => {console.log('')}}>
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
           <Icon name="log-in" size={20} color="#ff9000" />
           <CreateAccountButtonText>Create an account</CreateAccountButtonText>
         </CreateAccountButton>
