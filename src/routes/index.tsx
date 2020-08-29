9f5b440f17abcb4a08db2eaef30676e9410bab38
Mhayk Whandson <hi@mhayk.com>
2020-08-29 17:13:06 +0100

    The SignUp path component has been fixed.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../pages/SignIn'
-import SignUp from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
 
 const Auth = createStackNavigator();
 
