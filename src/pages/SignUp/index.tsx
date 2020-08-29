ab957ac64a4cf6bb0f81371819dd25fa55ad0d77
Mhayk Whandson <hi@mhayk.com>
2020-08-29 16:57:32 +0100

    The SignUp page has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Title,
+  BackToSignIn,
+  BackToSignInText
+ } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
   return (
-    <Container />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined }
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1}}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Create your account</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Name" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button onPress={() => { console.log('')}}>Enter</Button>
+
+          </Container>
+        </ScrollView>
+
+        <BackToSignIn onPress={() => {console.log('')}}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Back to Logon</BackToSignInText>
+        </BackToSignIn>
+      </KeyboardAvoidingView>
+    </>
   )
 }
 
