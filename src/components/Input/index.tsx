cb8f2fe0905beabe4263b27fe9d63beb423d3cf5
Mhayk Whandson <hi@mhayk.com>
2020-08-30 08:57:22 +0100

    The input component has been integrated with Unform.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,18 +9,50 @@ interface InputPros extends TextInputProps {
   icon: string;
 }
 
-const Input: React.FC<InputPros> = ({ name, icon, ...rest}) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
-    <TextInput
-      /**
-       * dark keyboard style works just to iOS
-       */
-      keyboardAppearance="dark"
-      placeholderTextColor="#666360"
-      {...rest}
-    />
-  </Container>
-)
+interface InputValueReference {
+  value: string;
+}
+
+const Input: React.FC<InputPros> = ({ name, icon, ...rest}) => {
+  const inputElementRef = useRef<any>(null);
+
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(ref: any, value) {
+        inputValueRef.current.value = value;
+        inputElementRef.current.setNativeProps({ text: value });
+      },
+      clearValue() {
+        inputValueRef.current.value = '';
+        inputElementRef.current.clear();
+      }
+    })
+  }, [fieldName, registerField])
+
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+      <TextInput
+        /**
+         * dark keyboard style works just to iOS
+         */
+        ref={inputElementRef}
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onChangeText={value => {
+          inputValueRef.current.value = value;
+        }}
+        {...rest}
+      />
+    </Container>
+  )
+}
 
 export default Input;
