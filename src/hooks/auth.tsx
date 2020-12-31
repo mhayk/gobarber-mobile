774b7c0c2eed7e006f41a06d2e5f4e7e14d35819
Mhayk Whandson <hi@mhayk.com>
2020-12-31 14:09:44 +0000

    A user interface has been created and the token has been added via default header in our service.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,9 +2,16 @@ import React, { createContext, useCallback, useState, useContext, useEffect } fr
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -13,7 +20,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -32,8 +39,10 @@ export const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user'
       ]);
 
-      if (token[1] && user[1])
+      if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({token: token[1], user: JSON.parse(user[1])})
+      }
 
       setLoading(false)
     }
@@ -52,6 +61,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)]
     ])
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
